64b303cf4825c7259f122e20e7590295324d8b95
abdelmomen1985 <abdelmomen1985@gmail.com>
2021-06-24 17:00:48 +0000

    Novi

--- api/strp-rooms/models/strp-rooms.js
+++ api/strp-rooms/models/strp-rooms.js
@@ -1,8 +1,21 @@
 'use strict';
-
+const slugify = require('slugify');
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    async beforeCreate(data) {
+      if (data.name.en) {
+        data.slug = slugify(data.name.en, {lower: true});
+      }
+    },
+    async beforeUpdate(params, data) {
+      if (data.name.en) {
+        data.slug = slugify(data.name.en, {lower: true});
+      }
+    },
+  },
+};
